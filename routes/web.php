f83069be56f97e44b6c6888d7d018af781dfefab
Fikri Ardiansyah <fikriardi2004@gmail.comgit config --global user.email fikriardi2004@gmail.comgit config --global user.email fikriardi2004@gmail.com>
2024-11-13 11:57:37 +0700

    Penambahan Page Home

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.login');
+});
+
+Route::get('/dashboard', function () {
+    return view('pages.auth.dashboard');
+});
+Route::get('/home', function () {
+    return view('pages.auth.home');
 });
